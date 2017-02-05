13d0a1a8156556c65cd16ee24681b7a86279fdcb
BlackFoxy757 <blackkiler1527@gmail.com>
2017-02-05 13:46:52 +0400

    Xenon-ify
    Rename lineage.dependencies to xenonhd.dependencies
    
    Rename xenon.mk to xenonhd.mk

--- xenonhd.mk
+++ xenonhd.mk
@@ -2,16 +2,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Inherit from device specific
 $(call inherit-product, device/asus/Z00ED/full_Z00ED.mk)
 
-PRODUCT_NAME := lineage_Z00ED
+PRODUCT_NAME := xenonhd_Z00ED
 PRODUCT_DEVICE := Z00ED
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone 2 Laser
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+# Root options (root apps = substratum, adaway, kernel adiutor)
+WITH_SUPERSU := true
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=starscream
